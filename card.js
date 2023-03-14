ec2e04956ecc87e769e4af6ab7f7d6adcda0ee06
MohonSaha <practicewithmohon@gmail.com>
2023-03-14 12:55:16 +0600

    local storage interactions for a shopping cart

--- card.js
+++ card.js
@@ -7,13 +7,47 @@ const addProduct = () => {
     quantityField.value = '';
 
     displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
 }
 
 
+// Function: To show items in the UI.
 const displayProduct = (product, quantity) => {
     const ul = document.getElementById('product-container');
     const li = document.createElement('li');
     li.innerText = `${product} : ${quantity}`;
     ul.appendChild(li);
-
 }
+
+
+// Function: To check local storage and get data from local storage.
+const getStoredShoppingCart = () =>{
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+
+// Function: To set data in local storage.
+const saveProductToLocalStorage = (product, quantity) =>{
+    const cart = getStoredShoppingCart();
+    cart [product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+
+
+// Function: To show data from local storage.
+const displayProductsFromLocalStorage = () =>{
+    const savedCart = getStoredShoppingCart();
+    for(const product in savedCart){
+        const quantity = savedCart[product];
+        displayProduct(product, quantity);
+    }
+}
+
+displayProductsFromLocalStorage();
